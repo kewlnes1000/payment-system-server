93226f04322042059b001c28d6449d249fd0e973
kewlnes2000 <kewlnes2000@gmail.com>
2019-11-10 12:17:27 +0800

    [Views]update frontend react

---
+++ public/precache-manifest.3f661e297b726d537caa9639e1c23f4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4590ce7d3bee338c884b99ca16a85700",
+    "url": "/index.html"
+  },
+  {
+    "revision": "084e07d9f31616c46519",
+    "url": "/static/css/main.2d2fa604.chunk.css"
+  },
+  {
+    "revision": "5c6b839f4dec14092a63",
+    "url": "/static/js/2.08937aed.chunk.js"
+  },
+  {
+    "revision": "084e07d9f31616c46519",
+    "url": "/static/js/main.7ed74d5b.chunk.js"
+  },
+  {
+    "revision": "5df88714cfaab7b25025",
+    "url": "/static/js/runtime-main.06a7e134.js"
+  }
+]);
